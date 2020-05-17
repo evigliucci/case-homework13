2616129ba5bd797e9d0d03956ef5da7e3bb388fc
Erika Vigliucci <evigliucci@gmail.com>
2020-05-17 11:48:01 -0400

    add JawsDb connection

--- config/connection.js
+++ config/connection.js
@@ -18,5 +18,19 @@ connection.connect(function(err) {
     console.log("MySql DB connected as id " + connection.threadId);
 });
 
+//Connection to JawsDB in Heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "cat_db"
+    });
+}
+
+
 // Export connection for our ORM to use.
 module.exports = connection;
