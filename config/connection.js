23683c9732b768e3e91660f75b06f220a327d536
Erika Vigliucci <evigliucci@gmail.com>
2020-05-17 12:02:27 -0400

    update jaws connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Hailey1$",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Hailey1$",
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -15,22 +20,8 @@ connection.connect(function(err) {
         console.error("error connecting: " + err.stack);
         return;
     }
-    console.log("MySql DB connected as id " + connection.threadId);
+    console.log("connected as id " + connection.threadId);
 });
-
-//Connection to JawsDB in Heroku
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "password",
-        database: "cat_db"
-    });
-}
-
 
 // Export connection for our ORM to use.
 module.exports = connection;
